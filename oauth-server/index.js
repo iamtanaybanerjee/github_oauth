4b2457b50148caf9aa07c5e5e8e90632095ac0b9
Tanay Banerjee <tanaybanerjee@Tanays-MacBook-Air.local>
2025-05-02 22:11:32 +0530

    feat: github oauth

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -13,9 +13,35 @@ app.get("/", async (req, res) => {
   return res.send(`<h1>Welcome to OAuth server!</h1>`);
 });
 
-app.get("/auth/gtihub", async (req, res) => {
+app.get("/auth/github", async (req, res) => {
   const gtihubAuthURL = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user,repo,security_events`;
   res.redirect(gtihubAuthURL);
 });
 
-app.get("/auth/github/callback");
+app.get("/auth/github/callback", async (req, res) => {
+  const { code } = req.query;
+  try {
+    const tokenResponse = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      {
+        headers: { Accept: "application/json" },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    res.cookie("access_token", accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/github`);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("server is listening to port 4000");
+});
