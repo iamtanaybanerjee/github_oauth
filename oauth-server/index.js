f766d1ccfe0ee584e361e4780d648962ac006bc7
Tanay Banerjee <tanaybanerjee@Tanays-MacBook-Air.local>
2025-05-05 22:54:18 +0530

    feat: google oauth added

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -20,6 +20,9 @@ app.get("/auth/github", async (req, res) => {
 
 app.get("/auth/github/callback", async (req, res) => {
   const { code } = req.query;
+  if (!code) {
+    return res.status(400).json.send("Authorization code not provided");
+  }
   try {
     const tokenResponse = await axios.post(
       "https://github.com/login/oauth/access_token",
@@ -39,6 +42,44 @@ app.get("/auth/github/callback", async (req, res) => {
     return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/github`);
   } catch (error) {
     return res.status(500).json({ error: error.message });
+  }
+});
+
+//google oauth
+app.get("/auth/google", (req, res) => {
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+
+  res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    return res.status(400).json.send("Authorization code not provided");
+  }
+
+  let accessToken;
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+      },
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+
+    accessToken = tokenResponse.data.access_token;
+    res.cookie("access_token", accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+  } catch (error) {
+    console.error(error);
   }
 });
 
