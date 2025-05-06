5fe588bc549d05dffaf54262568c34fb4830bc12
Tanay Banerjee <tanaybanerjee@Tanays-MacBook-Air.local>
2025-05-06 23:11:11 +0530

    feat: added protected routes with secure cookies

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -1,16 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const secureCookie = require("./services/index");
+const cookieParser = require("cookie-parser");
+const { verifyAccessToken } = require("./middleware/index");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
 
 app.get("/", async (req, res) => {
   return res.send(`<h1>Welcome to OAuth server!</h1>`);
+});
+
+app.get("/user/profile/github", verifyAccessToken, async (req, res) => {
+  console.log("Reached /user/profile/github route");
+  try {
+    const { access_token } = req.cookies;
+    const githubUserResponse = await axios.get("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    res.json({ user: githubUserResponse.data });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/user/profile/google", verifyAccessToken, async (req, res) => {
+  try {
+    const { access_token } = req.cookies;
+    const googleUserResponse = await axios.get(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    res.json({ user: googleUserResponse.data });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.get("/auth/github", async (req, res) => {
@@ -38,8 +75,9 @@ app.get("/auth/github/callback", async (req, res) => {
 
     const accessToken = tokenResponse.data.access_token;
 
-    res.cookie("access_token", accessToken);
-    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/github`);
+    // res.cookie("access_token", accessToken);
+    secureCookie(res, accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v2/profile/github`);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
@@ -76,8 +114,9 @@ app.get("/auth/google/callback", async (req, res) => {
     );
 
     accessToken = tokenResponse.data.access_token;
-    res.cookie("access_token", accessToken);
-    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+    // res.cookie("access_token", accessToken);
+    secureCookie(res, accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v2/profile/google`);
   } catch (error) {
     console.error(error);
   }
